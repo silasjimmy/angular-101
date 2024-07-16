66be0932fb139bdbcfd2c478dbb4042b626af339
Silas Jimmy <jimmysilas17@gmail.com>
2024-07-16 04:20:39 +0300

    refactor: create components

---
+++ app/src/app/to-do-list-item/to-do-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './to-do-list-item.component.html',
+  styleUrl: './to-do-list-item.component.css'
+})
+export class ToDoListItemComponent {
+
+}
